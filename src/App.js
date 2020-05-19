44d2efdd338fc07581121d12c7368d0c62aeae85
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2020-05-19 21:59:19 +0300

    Split into components resume

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Title from "./components/Title";
+import ContentBlock from "./components/ContentBlock";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Header">
+        <div className="Header__content">
+          <Title level="9">Sophie Alpert</Title>
+          <div className="black-box">Programmer</div>
+        </div>
       </header>
+      <main>
+        <ContentBlock className="col-12" title="About Me">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut justo libero.
+          Vestibulum vitae mattis diam. Vivamus eleifend diam vel tempor lacinia. Suspendisse non
+          augue egestas, dapibus justo et, lobortis ex. Nullam tortor diam, venenatis at enim a,
+          lacinia porttitor erat. Vivamus tempor dictum leo id aliquam. Praesent elit lacus, tempus
+          ac vehicula in, imperdiet dapibus elit. Nullam scelerisque euismod leo id vestibulum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut justo libero.
+          Vestibulum vitae mattis diam.
+        </ContentBlock>
+      </main>
     </div>
   );
 }
