eabd20bd58480eca5a214e4feeaf82b1a039afb5
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2020-05-20 21:59:59 +0300

    Add state examples

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
-import "./App.css";
+// import SVG from "react-inlinesvg";
+import { FaBeer } from "react-icons/fa";
 
+import "./App.css";
 import Title from "./components/Title";
 import ContentBlock from "./components/ContentBlock";
 
+// import devTo from "./images/social/dev-to.svg";
+
+let username = null;
+let password = null;
+
 function App() {
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    // console.dir(event.target);
+    alert(`username: ${username} and password: ${password}`);
+  };
+
   return (
     <div className="App">
       <header className="Header">
@@ -13,7 +27,31 @@ function App() {
           <div className="black-box">Programmer</div>
         </div>
       </header>
+      <div style={{ margin: "20px" }}>
+        <form onSubmit={onSubmit}>
+          <input
+            onChange={(event) => {
+              username = event.target.value;
+            }}
+            type="text"
+            placeholder="username"
+          />
+          <input
+            onChange={(event) => {
+              password = event.target.value;
+            }}
+            type="password"
+            placeholder="password"
+          />
+          <button disabled={!username || !password} type="submit">
+            Click Me
+          </button>
+        </form>
+      </div>
       <main>
+        {/* <div className="Social__icon">
+          <SVG className="SVG" src={devTo} />
+        </div> */}
         <ContentBlock className="col-12" title="About Me">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut justo libero.
           Vestibulum vitae mattis diam. Vivamus eleifend diam vel tempor lacinia. Suspendisse non
@@ -23,6 +61,9 @@ function App() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut justo libero.
           Vestibulum vitae mattis diam.
         </ContentBlock>
+        <div className="Social__icon">
+          <FaBeer />
+        </div>
       </main>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+function P({ children }) {
+  return <p>{children}</p>;
+}
+
+class CounterV1 extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     count: this.props.count || 0,
+  //     text: "",
+  //   };
+  // }
+
+  state = {
+    count: this.props.count || 0,
+    text: "",
+  };
+
+  render() {
+    const { count, text } = this.state;
+
+    return (
+      <div>
+        <P>{count}</P>
+        <p>Text is: {text}</p>
+        <input onChange={(event) => this.setState({ text: event.target.value })} />
+        <button disabled={!text} type="button" onClick={() => this.setState({ count: count + 1 })}>
+          Add
+        </button>
+      </div>
+    );
+  }
+}
+
+function CounterV2(props) {
+  const [count, setCount] = useState(props.count);
+  const [text, setText] = useState("Hello");
+  const [moreText, setMoreText] = useState("more");
+
+  return (
+    <div>
+      <p>{count}</p>
+      <p>{text}</p>
+      {count >= 5 && <p>{moreText}</p>}
+      <input onChange={(event) => setText(text + event.target.value)} />
+      <button type="button" onClick={() => setCount(count + 1)}>
+        Add
+      </button>
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <React.Fragment>
+    <CounterV1 count={5} />
+    <hr />
+    <CounterV2 count={9} />
+    <hr />
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById("root")
 );
 
